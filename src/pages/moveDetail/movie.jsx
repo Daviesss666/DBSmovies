a1da952f4709b6ff93db92ca34c16097622f5a34
Daviesss666 <sevilladavebryan@gmail.com>
2023-08-18 21:16:11 +0800

    changes

--- src/pages/moveDetail/movie.jsx
+++ src/pages/moveDetail/movie.jsx
@@ -61,15 +61,7 @@ const Movie = () => {
                 </div>
             </div>
             <div>{currentMovieDetail ? currentMovieDetail.cast : ""}</div>
-            <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
+            
             <div className="movie__heading">Production companies</div>
             <div className="movie__production">
                 {
